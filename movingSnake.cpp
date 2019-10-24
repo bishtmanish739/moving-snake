586a65e7bd2e91e190283bab7ddfe6021905c22b
Mahavir Singh <bishtmanish739@gmail.com>
2019-10-24 15:03:26 +0530

    moving snake code is added

---
+++ movingSnake.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+int main(){
+
+int screensize=100;
+int space=0;
+while(space<=screensize){
+for(int i=0;i<=space;i++){
+    cout<<" ";
+
+}
+cout<<"---@";
+for(int l=0;l,l<10000000;l++);
+system("cls");
+//for(int l=0;l,l<10000000;l++);
+space++;
+}
+screensize=25;
+int j=screensize;
+for(int i=0;i<screensize;i++){
+        for(int l=0;l<=i;l++){
+            cout<<endl;
+        }
+            for(int k=0;k<space;k++){
+
+                    cout<<" ";
+
+            }
+            cout<<"|"<<endl;
+        for(int k=0;k<space;k++){
+            cout<<" ";
+        }
+            cout<<"|";
+            cout<<endl;
+            for(int k=0;k<space;k++){
+            cout<<" ";
+        }cout<<"@";
+            //cout<<endl;
+            system("cls");
+
+        }
+cout<<"****************THANKS GOD SNAKE HAS GONE press x to exit";cout<<endl;cout<<"*********************************";
+cin>>screensize;
+
+
+}
